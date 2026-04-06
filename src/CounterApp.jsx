dbfbfadea51ff6a5194c181f989e1883eb6fed07
dnswjdfb <yjhgg1809@gmail.com>
2026-04-06 13:47:17 +0900

    ConterApp+(최대10까지)

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,16 +5,22 @@ import heroImg from './assets/hero.png'
 // import './App.css'
 
 function CounterApp() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)//상태 . 기본값:0.count:이전 또는 현재 값, setCount: count 값 변경 함수
+  //count 값을 변경할 때 setCount()를 사용해서 변경해야 함!! => react가 count 상태변화를 눈치 챔
 
   return (
     <>
-    <button
+    <h1>{count}</h1>
+    <button onClick={() => setCount(count + 1)}>+</button>
+    <button onClick={() => setCount(count - 1)}>-</button>
+    <button onClick={()=>setCount(Math.min(count + 1, 10))}>max(10)</button>
+    <button onClick={() => setCount(0)}>reset</button>
+    {/* <button
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
-        </button>
+        </button> */}
     </>
   )
 }
